7d1b98180176abc5b2899d1b4ce5d584f9151159
MuteBard <csevere@brandeis.edu>
2018-02-24 21:17:19 -0500

    ready contentcard4 done, needs images

--- src/Components/ContentCard4.js
+++ src/Components/ContentCard4.js
@@ -5,14 +5,16 @@ class ContentCard4 extends Component {
     render() {
         return (
             <div className="contentcard4">
-                <div className="cc4box cc4b1">[LOGO HERE]</div>
+                <div className="cc4box cc4b1">
+                    <div className="cc4innerb1">02</div>
+                    <div className="cc4innerb2">PORTFOLIO</div>
+                </div>
                 <div className="cc4box cc4b2">
-                    <div className="cc4innerb1">Hello</div>
-                    <div className="cc4innerb2">Welcome to my page. I am a UX/UI Designer and Developer with 2 years of experience. My expertise ranges from bootstrap coding to user-centered design with react-native components.</div>
+                    <div className="cc4innerb3">[IMAGE A]</div>
+                    <div className="cc4innerb4">[IMAGE B]</div>
+                    <div className="cc4innerb5">[IMAGE C]</div>
                 </div>
-                <div className="cc4box cc4b3">
-                    <div className="cc4innerb3">DOWNLOAD RESUME</div>
-                </div>
+
             </div>
         );
     }

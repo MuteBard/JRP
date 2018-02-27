ae13cf300c864b600e7afa16022aa17b4b2c8fdc
MuteBard <csevere@brandeis.edu>
2018-02-27 00:21:37 -0500

    input fields done

--- src/Components/ContentCard6.js
+++ src/Components/ContentCard6.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import '../app.scss';
 
+//https://reactjs.org/docs/forms.html
+//https://alligator.io/react/new-way-to-handle-events/
 class ContentCard6 extends Component {
+    constructor(){
+        super();
+        this.state = {
+          name: '',
+          email: '',
+          subject: '',
+          message: ''  
+        }
+    }
+
+    edit = (item) => (event) => {
+        this.setState({[item]: event.target.value}) 
+    }
+
+    saveEdit(event){
+
+    }
+
     render() {
         return (
             <div className="contentcard6">
@@ -16,19 +36,19 @@ class ContentCard6 extends Component {
                         <div className="cc6lvl3b2">
                             <div className="cc6lvl4b5">
                                 <div className="cc6lvl5b1">
-                                    <input className="searchb1"  placeholder="Name*" type="text" value={"Name*"}/>
+                                    <input className="searchb1" placeholder="Name*" type="text" onChange = {this.edit('name')} value = {this.state.name}/>
                                 </div>
                                 <div className="cc6lvl5b2">
-                                    <input className="searchb1"  placeholder="Email*" type="text" value={"Email*"}/>
+                                    <input className="searchb1" placeholder="Email*" type="text" onChange = {this.edit('email')} value = {this.state.email}/>
                                 </div>
                             </div>
                             <div className="cc6lvl4b6">
-                                <input className="searchb2"  placeholder="Subject" type="text" value={"Subject"}/>
+                                <input className="searchb2" placeholder="Subject*" type="text" onChange = {this.edit('subject')} value = {this.state.subject}/>
                             </div>
                             <div className="cc6lvl4b7">
-                                <textarea className="searchb3" placeholder="Message" cols="40" rows="5" value={"Message"}></textarea>
+                                <textarea className="searchb3" placeholder="Message*" cols="40" rows="5" onChange = {this.edit('message')} value = {this.state.message}/>
                             </div>
-                            <div className="cc6lvl4b8">Send</div>
+                            <button className="cc6lvl4b8" type="button" onClick={this.saveEdit}>Send</button>
                         </div>
                     </div>
                     <div className="cc6lvl2b2">
